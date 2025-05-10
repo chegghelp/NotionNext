b6a12f688de51c84dec29bcbaa1a726a75203095
chegghelp <89557197+chegghelp@users.noreply.github.com>
2025-05-10 08:26:12 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'LearnSelect', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '老师信赖，学生受益', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://studue.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'teaching resources, Study Materials', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
